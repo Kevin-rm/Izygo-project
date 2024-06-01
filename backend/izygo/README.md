cb498aacc88cf8303c152955e6282518428c29b1
Ramavo Harinaivo <nyxrafaly@gmail.com>
2024-06-01 11:43:33 +0300

    readme sy data_test.sql

---
+++ backend/izygo/README.md
@@ -0,0 +1,16 @@
+# IZY GO
+
+## Note
+
+- Pour recharger la dépendance executer la commande
+  - Si sous linux ou terminal visual code
+  
+    ```bash
+    ./mvnw dependency:resolve
+    ```
+
+  - Si cmd Windows
+
+    ```bash
+    mvwn.cmd dependency:resolve
+    ```
---
+++ backend/izygo/src/main/resources/sql/data_test.sql
@@ -0,0 +1,44 @@
+
+-- Insérer les arrêts spécifiques pour la ligne 137
+
+
+-- Insérer la ligne 137
+INSERT INTO line (label) VALUES 
+('ligne A'),
+('ligne B'),
+('ligne C');
+
+
+INSERT INTO stop (label) VALUES 
+('Andoharanofotsy'),
+('Malaza'),
+('Magasin M-'),
+('Tanjombato'),
+('Fasika'),
+('Ankandimbahoaka'),
+('Descours'),
+('Paraky'),
+('Toby'),
+('Anosy'),
+
+('Ambatomaro'),
+('Mahazo'),
+('Ankadindramamy'),
+('Pain de Tana'),
+('Meteo'),
+('Avaradoha'),
+('Betongolo'),
+('Antsakaviro'),
+('Ambohijatovo'),
+('Ampefiloha'),
+('67ha'),
+('Antanimena'),
+('Ankakifotsy'),
+('Andravoahangy ambony'),
+('Besarety'),
+
+
+('Tsena Andravoahangy'),
+('Ambany Tetezana - Behoririka'),
+('Soarana');
+
