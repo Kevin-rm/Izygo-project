dedc7d9793af411deaeb7916e8c4510924fa581c
Kevin-rm <kevinramarozatovo@gmail.com>
2024-06-05 21:13:33 +0300

    Update queries.sql

--- backend/izygo/src/main/resources/sql/queries.sql
+++ backend/izygo/src/main/resources/sql/queries.sql
@@ -22,7 +22,7 @@ FROM line_stop ls
      stop s ON ls.stop_id = s.id
 ORDER BY l.id;
 
--- Meilleur affichage du trajet de chaque ligne
+-- Meilleur affichage du trajet de chaque ligne mais pas en ordre
 CREATE OR REPLACE VIEW v_line_path AS
 SELECT lp.line_id,
        vls_1.line_label,
@@ -37,11 +37,33 @@ FROM line_path lp
          JOIN
      v_line_stop vls_1 ON lp.line_id = vls_1.line_id AND lp.from_stop_id = vls_1.stop_id
          JOIN
-     v_line_stop vls_2 ON lp.line_id = vls_2.line_id AND lp.to_stop_id = vls_2.stop_id
-ORDER BY lp.line_id,
-         CASE
-             WHEN lp.from_stop_id < lp.to_stop_id THEN lp.from_stop_id
-             END,
-         CASE
-             WHEN lp.from_stop_id > lp.to_stop_id THEN lp.from_stop_id
-             END DESC;
+     v_line_stop vls_2 ON lp.line_id = vls_2.line_id AND lp.to_stop_id = vls_2.stop_id;
+
+/*
+ * Permet de retrouver tous les itinéraires possibles en donnant
+ * l'arrêt de départ et l'arrêt d'arrivée
+ */
+WITH RECURSIVE path_finding AS (
+    SELECT lp.from_stop_id,
+           lp.to_stop_id,
+           ARRAY [lp.from_stop_id, lp.to_stop_id] AS path,
+           lp.estimated_duration                  AS total_duration
+    FROM line_path lp
+    WHERE lp.from_stop_id = 1
+
+    UNION ALL
+
+    SELECT pf.from_stop_id,
+           lp.to_stop_id,
+           pf.path || lp.to_stop_id,
+           pf.total_duration + lp.estimated_duration
+    FROM line_path lp
+             INNER JOIN
+         path_finding pf ON lp.from_stop_id = pf.to_stop_id
+    WHERE lp.to_stop_id <> ALL (pf.path)
+)
+SELECT path,
+       total_duration
+FROM path_finding
+WHERE to_stop_id = 15
+ORDER BY total_duration;
