76f37c2f47068ed599300dc1efa941d0a2ff6447
FiononantsoaF <153853648+FiononantsoaF@users.noreply.github.com>
2024-06-06 11:52:41 +0200

    reservation

--- backend/izygo/src/main/resources/sql/data_test.sql
+++ backend/izygo/src/main/resources/sql/data_test.sql
@@ -2,6 +2,8 @@ INSERT INTO "roles" ("type") VALUES ('user');
 INSERT INTO "roles" ("type") VALUES ('backoffice');
 
 
+
+
 INSERT INTO line (label)
 VALUES ('Ligne A'),
        ('Ligne B'),
@@ -38,5 +40,41 @@ VALUES ('Andoharanofotsy'),
        ('Tsena Andravoahangy'),
        ('Ambany Tetezana - Behoririka'),
        ('Soarano');
+INSERT INTO bus(license_plate,number_of_seats,line_id)
+VALUES('3652TCA',20,1),
+      ('3620TCA',20,2),
+      ('9842TCA',20,3);
+
+INSERT INTO seat(label)
+VALUES('a1'),
+      ('a2'),
+      ('a3'),
+      ('a4');
+
+INSERT INTO bus_seat(bus_id,seat_id)
+VALUES(1,1),
+      (1,2),
+      (1,3),
+      (1,4),
+      (2,1),
+      (2,2),
+      (2,3),
+      (2,4),
+      (3,1),
+      (3,2),
+      (3,3),
+      (3,4);
 
 
+INSERT INTO reservation(date_time,user_id,bus_id)
+VALUES('2024-05-06 10:20:00',1,1),
+      ('2024-05-06 11:20:00',1,1),
+      ('2024-05-06 8:20:00',3,2),
+      ('2024-05-10 7:20:00',3,3);
+
+
+INSERT INTO reservation_seat(seat_id,reservation_id)
+VALUES(1,1),
+      (2,2),
+      (3,2),
+      (4,2);
--- backend/izygo/src/main/resources/sql/queries.sql
+++ backend/izygo/src/main/resources/sql/queries.sql
@@ -1,9 +1,28 @@
-CREATE OR REPLACE VIEW vlist_Admin AS 
+CREATE OR REPLACE VIEW v_list_Admin AS 
 SELECT phone_Number 
 FROM "user" 
 WHERE "role_id" = 2;
 
 ----------------par id reservation (liste siege an reservation ray)
 --------------par id user ,liste reservation (user_id,resv_id,)
+SELECT 
+    r.user_id AS user_id,
+    r.id AS reservation_id,
+    r.date_time AS reservation_date,
+    COUNT(rs.seat_id) AS number_of_seats,
+    l.label AS bus_line
+FROM 
+    reservation r
+JOIN 
+    bus b ON r.bus_id = b.id
+JOIN 
+    "line" l ON b.line_id = l.id
+JOIN 
+    reservation_seat rs ON r.id = rs.reservation_id
+GROUP BY 
+    r.id, r.date_time, l.label;
+
+
+
 
 
