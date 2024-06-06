b86675832e5bf9e393c47894f13dfd6e984c7349
FiononantsoaF <153853648+FiononantsoaF@users.noreply.github.com>
2024-06-06 10:36:04 +0200

    reservation

--- backend/izygo/src/main/resources/sql/data_test.sql
+++ backend/izygo/src/main/resources/sql/data_test.sql
@@ -2,3 +2,41 @@ INSERT INTO "roles" ("type") VALUES ('user');
 INSERT INTO "roles" ("type") VALUES ('backoffice');
 
 
+INSERT INTO line (label)
+VALUES ('Ligne A'),
+       ('Ligne B'),
+       ('Ligne C');
+
+INSERT INTO stop (label)
+VALUES ('Andoharanofotsy'),
+       ('Malaza'),
+       ('Magasin M-'),
+       ('Tanjombato'),
+       ('Fasika'),
+       ('Ankandimbahoaka'),
+       ('Descours'),
+       ('Paraky'),
+       ('Toby'),
+       ('Anosy'),
+
+       ('Ambatomaro'),
+       ('Mahazo'),
+       ('Ankadindramamy'),
+       ('Pain de Tana'),
+       ('Meteo'),
+       ('Avaradoha'),
+       ('Betongolo'),
+       ('Antsakaviro'),
+       ('Ambohijatovo'),
+       ('Ampefiloha'),
+       ('67ha'),
+       ('Antanimena'),
+       ('Ankadifotsy'),
+       ('Andravoahangy ambony'),
+       ('Besarety'),
+
+       ('Tsena Andravoahangy'),
+       ('Ambany Tetezana - Behoririka'),
+       ('Soarano');
+
+
