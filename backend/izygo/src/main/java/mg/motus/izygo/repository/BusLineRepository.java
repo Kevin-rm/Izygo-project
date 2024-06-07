68c8077e8c5a48b4a0dc5725fcb69af0b97fe3cf
Ramavo Harinaivo <nyxrafaly@gmail.com>
2024-06-07 11:18:04 +0300

    adding BusLineRepository
    adding cancellation sy reservatio

---
+++ backend/izygo/src/main/java/mg/motus/izygo/repository/BusLineRepository.java
@@ -0,0 +1,8 @@
+package mg.motus.izygo.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import mg.motus.izygo.model.BusLine;
+
+public interface BusLineRepository extends CrudRepository<BusLine,Long>{
+}
--- backend/izygo/src/main/java/mg/motus/izygo/service/ReservationService.java
+++ backend/izygo/src/main/java/mg/motus/izygo/service/ReservationService.java
@@ -1,15 +1,43 @@
 package mg.motus.izygo.service;
 
+import mg.motus.izygo.model.Reservation;
+import mg.motus.izygo.model.ReservationSeat;
 import mg.motus.izygo.repository.ReservationRepository;
+import mg.motus.izygo.repository.ReservationSeatRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReservationService {
     private ReservationRepository reservationRepository;
+    private ReservationSeatRepository reservationSeatRepository;
 
     @Autowired
-    public ReservationService(ReservationRepository reservationRepository) {
+    public ReservationService(ReservationRepository reservationRepository,ReservationSeatRepository reservationSeatRepository) {
         this.reservationRepository = reservationRepository;
+        this.reservationSeatRepository = reservationSeatRepository;
     }
+
+    public Reservation createReservation(Long userId, Long busId, List<Short> seatIds) {
+        Reservation reservation = Reservation.builder()
+        .busId(busId)
+        .userId(userId)
+        .dateTime(LocalDateTime.now())
+        .build();
+        reservation = reservationRepository.save(reservation);
+
+        for (Short seatId : seatIds) {
+            ReservationSeat reservationSeat = ReservationSeat.builder()
+            .reservationId(reservation.getId())
+            .seatId(seatId)
+            .build();
+            reservationSeatRepository.save(reservationSeat);
+        }
+        return reservation;
+    }
+
 }
