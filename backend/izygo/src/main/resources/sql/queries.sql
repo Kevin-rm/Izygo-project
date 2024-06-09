46f7f2f56815b0532c514f1d8f927668775e4257
FiononantsoaF <153853648+FiononantsoaF@users.noreply.github.com>
2024-06-09 21:10:26 +0200

    reservation

--- backend/izygo/src/main/resources/sql/queries.sql
+++ backend/izygo/src/main/resources/sql/queries.sql
@@ -1,16 +1,19 @@
+-- view list_admin
+
 CREATE OR REPLACE VIEW v_list_Admin AS 
 SELECT phone_Number 
 FROM "user" 
 WHERE "role_id" = 2;
 
-----------------par id reservation (liste siege an reservation ray)
---------------par id user ,liste reservation (user_id,resv_id,)
+--view list reservation par user_id
+CREATE OR REPLACE VIEW v_list_reservation AS
 SELECT 
     r.user_id AS user_id,
     r.id AS reservation_id,
     r.date_time AS reservation_date,
     COUNT(rs.seat_id) AS number_of_seats,
-    l.label AS bus_line
+    l.label AS bus_line,
+    r.bus_id AS bus_id
 FROM 
     reservation r
 JOIN 
@@ -20,7 +23,32 @@ JOIN
 JOIN 
     reservation_seat rs ON r.id = rs.reservation_id
 GROUP BY 
-    r.id, r.date_time, l.label;
+    r.user_id, r.id, r.date_time, l.label;   
+
+--view liste seat pour chaque reservation /user_id (reservation active)  
+CREATE OR REPLACE VIEW v_list_reservation_seat AS
+SELECT
+    vr.user_id AS user_id,
+    vr.reservation_id AS reservation_id,
+    vr.reservation_date AS reservation_date,
+    vr.bus_line AS bus_line,
+    s.label AS seat_label
+FROM 
+    v_list_reservation vr
+JOIN 
+    bus b ON vr.bus_id = b.id
+JOIN
+    reservation_seat revs ON revs.reservation_id = vr.reservation_id  
+JOIN 
+    seat s ON revs.seat_id = s.id          
+GROUP BY 
+    vr.user_id, vr.reservation_id, vr.reservation_date, vr.bus_line, s.label, revs.seat_id;
+
+     
+
+
+
+
 
 
 
