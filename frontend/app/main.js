5870fa9d069c4f31cca881ae1138ef60e091e077
Kevin-rm <kevinramarozatovo@gmail.com>
2024-06-19 19:06:32 +0300

    suppress all the unecessary files

--- frontend/app/main.js
+++ frontend/app/main.js
@@ -1,20 +1,26 @@
-const app = angular.module('myApp', ['ngRoute']);
+const app = angular.module("izygoApp", ["ngRoute"]);
 
 app.config(function($routeProvider) {
-    $routeProvider
-    .when('/', {
-        templateUrl: 'views/login.html',
+        $routeProvider
+            .when("/", {
+                templateUrl: "views/login.html",
+            })
+            .when("/signin", {
+                templateUrl: "views/signin.html",
+            })
+            .when("/landing-page", {
+                templateUrl: "views/landing-page.html",
+            })
+            .when("/profile", {
+                templateUrl: "views/profile.html",
+            })
+            .otherwise({
+                redirectTo: "/"
+            });
     })
-    .when('/signin', {
-        templateUrl: 'views/signin.html',
-    })
-    .when('/landing-page', {
-        templateUrl: 'views/landing-page.html',
-    })
-    .when('/profile', {
-        templateUrl: 'views/profile.html',
-    })
-    .otherwise({
-        redirectTo: '/'
+    .filter("uppercase", function () {
+        return function (input) {
+            if (input) return input.toUpperCase();
+            return input;
+        };
     });
-});
