4b0885abba072edb0e94119f8592bd546a6614bd
Kevin-rm <kevinramarozatovo@gmail.com>
2024-06-22 19:35:26 +0300

    Update UserService.java

--- backend/izygo/src/main/java/mg/motus/izygo/service/UserService.java
+++ backend/izygo/src/main/java/mg/motus/izygo/service/UserService.java
@@ -5,13 +5,10 @@ import mg.motus.izygo.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import jakarta.validation.Valid;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository  = userRepository;
