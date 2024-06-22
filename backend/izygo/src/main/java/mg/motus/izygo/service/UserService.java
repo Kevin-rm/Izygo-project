71701cd181c673b2137dc335ace0eb6dc674899d
FiononantsoaF <153853648+FiononantsoaF@users.noreply.github.com>
2024-06-23 00:21:29 +0200

    integrationphonenumber

--- backend/izygo/src/main/java/mg/motus/izygo/service/UserService.java
+++ backend/izygo/src/main/java/mg/motus/izygo/service/UserService.java
@@ -2,8 +2,15 @@ package  mg.motus.izygo.service;
 
 import mg.motus.izygo.model.User;
 import mg.motus.izygo.repository.UserRepository;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import java.util.Set;
 
 import jakarta.validation.Valid;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,17 +20,28 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final Validator validator;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository  = userRepository;
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, Validator validator) {
+        this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+        this.validator = validator;
     }
 
     public User register(User user) {
-        user.setPassword(
-            passwordEncoder.encode(user.getPassword())
-        );
-
+        Set<ConstraintViolation<User>> violations = validator.validate(user);
+        if (!violations.isEmpty()) {
+            throw new ConstraintViolationException(violations);
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
+
+    // public User register(User user) {
+    //     user.setPassword(
+    //         passwordEncoder.encode(user.getPassword())
+    //     );
+
+    //     return userRepository.save(user);
+    // }
 }
