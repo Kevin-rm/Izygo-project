5b99a5c643a58594beb0e72d94a376e772f8b281
Ramavo Harinaivo <nyxrafaly@gmail.com>
2024-06-22 19:10:47 +0300

    Update TicketService.java

--- backend/izygo/src/main/java/mg/motus/izygo/service/TicketService.java
+++ backend/izygo/src/main/java/mg/motus/izygo/service/TicketService.java
@@ -37,11 +37,6 @@ public class TicketService{
     private String pathImg;
 
     public void addTicketInfo() {
-        // Configure Thymeleaf template resolver
-        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        templateResolver.setPrefix("/qr_ressources/Ticket/");
-        templateResolver.setSuffix(".html");
-        templateResolver.setTemplateMode("HTML");
 
         // Créer le QR Code
         String reservationSeatId = getReservationDTO().reservationSeatId().toString();
@@ -56,18 +51,84 @@ public class TicketService{
             e.printStackTrace();
         }
 
-        // Créer le moteur de template Thymeleaf
-        TemplateEngine templateEngine = new TemplateEngine();
-        templateEngine.setTemplateResolver(templateResolver);
-
-        // Créer le contexte et ajouter des variables
-        Context context = new Context();
-        context.setVariable("reservation", getReservationDTO());
-        context.setVariable("qrCode", filename);
-
-        // Générer le contenu HTML
-        String htmlContent = templateEngine.process("ticket", context);
-
+        // Contenu HTML avec formatage dynamique
+        String htmlContent = String.format(
+                "<!DOCTYPE html>\r\n" +
+                "<html lang=\"en\" xmlns:th=\"http://www.thymeleaf.org\">\r\n" +
+                "<head>\r\n" +
+                "    <meta charset=\"UTF-8\"></meta>\r\n" +
+                "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\"></meta>\r\n" +
+                "    <title>Ticket</title>\r\n" +
+                "    <link rel=\"stylesheet\" href=\"assets/css/bootstrap.min.css\"></link>\r\n" +
+                "    <link rel=\"stylesheet\" href=\"assets/css/template.css\"></link>\r\n" +
+                "</head>\r\n" +
+                "<body>\r\n" +
+                "    <div id=\"getimg\" class=\"container\">\r\n" +
+                "        <div class=\"row mt-2 mb-3 d-flex flex-row align-items-center\">\r\n" +
+                "            <div class=\"col-6\">\r\n" +
+                "                <p class=\"mx-2\">\r\n" +
+                "                    Ticket #%s\r\n" +
+                "                </p>\r\n" +
+                "            </div>\r\n" +
+                "            <div class=\"col-6 d-flex justify-content-end logo\"><img src=\"assets/icons/logo.png\" alt=\"\"></img></div>\r\n" +
+                "        </div>\r\n" +
+                "        <div class=\"container bg-dark text-white rounded-4 px-3 py-2 mb-2\">\r\n" +
+                "            <div class=\"px-3\">\r\n" +
+                "                <h6 class=\"fw-bolder title\">%s %s</h6>\r\n" +
+                "                <p>%s</p>\r\n" +
+                "            </div>\r\n" +
+                "            <hr></hr>\r\n" +
+                "            <div class=\"container\">\r\n" +
+                "                <div class=\"row justify-content-between\">\r\n" +
+                "                    <div class=\"col-5\">\r\n" +
+                "                        <center class=\"fw-bold\">Matricule</center>\r\n" +
+                "                        <center>%s</center>\r\n" +
+                "                    </div>\r\n" +
+                "                    <div class=\"col-5\">\r\n" +
+                "                        <center class=\"fw-bold\">Numero de siège</center>\r\n" +
+                "                        <center>%s</center>\r\n" +
+                "                    </div>\r\n" +
+                "                </div>\r\n" +
+                "                <div class=\"row justify-content-between\">\r\n" +
+                "                    <div class=\"col-5\">\r\n" +
+                "                        <center class=\"fw-bold\">Date de réservation</center>\r\n" +
+                "                        <center>01/04/2024</center>\r\n" +
+                "                    </div>\r\n" +
+                "                    <div class=\"col-5\">\r\n" +
+                "                        <center class=\"fw-bold\">Heure de départ</center>\r\n" +
+                "                        <center>12:45:00</center>\r\n" +
+                "                    </div>\r\n" +
+                "                </div>\r\n" +
+                "            </div>\r\n" +
+                "        </div>\r\n" +
+                "        <div class=\"container\">\r\n" +
+                "            <div class=\"row\">\r\n" +
+                "                <div class=\"col-5\">\r\n" +
+                "                    <center class=\"fw-bold\">Départ</center>\r\n" +
+                "                    <center>%s</center>\r\n" +
+                "                </div>\r\n" +
+                "                <div class=\"col-2 d-flex justify-content-center align-items-center\">\r\n" +
+                "                    <img src=\"assets/icons/bus-front-fill.svg\"alt=\"Bus Icon\"></img>\r\n" +
+                "                </div>\r\n" +
+                "                <div class=\"col-5\">\r\n" +
+                "                    <center class=\"fw-bold\">Arrivée</center>\r\n" +
+                "                    <center>%s</center>\r\n" +
+                "                </div>\r\n" +
+                "            </div>\r\n" +
+                "            <div class=\"d-flex justify-content-center align-items-center my-1\" >\r\n" +
+                "                <img src=\"%s\" alt=\"\" width=\"100\" style=\"width: 100px; height: 100px;\"></img>\r\n" +
+                "            </div>\r\n" +
+                "            <div class=\"text-center\">\r\n" +
+                "                <p class=\"faded\">Merci d'avoir choisi IzyGo by Motus</p>\r\n" +
+                "            </div>\r\n" +
+                "        </div>\r\n" +
+                "    </div>\r\n" +
+                "</body>\r\n" +
+                "</html>",
+                reservationDTO.id().toString(), reservationDTO.firstname(), reservationDTO.lastname(), reservationDTO.lineLabel(), 
+                reservationDTO.licensePlate(), reservationDTO.seatLabel(), reservationDTO.startStop(), reservationDTO.endStop(), filename
+        );
+        
         pathHtml = "src/main/resources/qr_ressources/Ticket/" + htmlPath;
         System.out.println("pathHtml initialisé: " + pathHtml);
 
